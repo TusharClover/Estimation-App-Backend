6c78340a4f1bea7e4acc2104f6b9eccc055ce918
Tushar Murkar <Tushar.Murkar@CTPL.in>
2025-02-28 10:59:51 +0530

    api changes in dev efforts

--- backend/app/controllers/estimationController.js
+++ backend/app/controllers/estimationController.js
@@ -55,10 +55,9 @@ const createEstimation = async(req, res) => {
 
                     const result = await Estimations.createEstimation(req.body);
                     if (result) {
-                        return res.status(200).send({ message: 'Estimations created successfully!', result: result.insertId ? true : false, success: true });
+                        return res.status(200).send({ message: 'Estimations created successfully!', result: result.insertId, success: true });
                     } else {
-                        console.log(result);
-                        return res.status(200).send({ error: 'Something went wrong, please check data properly', result: false, success: false });
+                        return res.status(200).send({ error: 'Something went wrong, please check data properly', result: 0, success: false });
                     }
                 } catch (refreshErr) {
                     console.log(refreshErr, "resfresh Error");
